292f51fbbca145370aea5ebf49819ec8b9993321
alien <15080165456@163.com>
2019-08-18 23:39:39 +0800

    Initial commit

---
+++ src/main/java/com/alien/Application.java
@@ -0,0 +1,19 @@
+package com.alien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: springbootRedis
+ * @description: Spring Boot 启动类
+ * @author: alien
+ * @since: 2019/08/18 23:28
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
